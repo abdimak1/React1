d31597748ede9a7b574a31e50ff64759abd3ec12
Abdirohman Mekonen <79455537+abdimak1@users.noreply.github.com>
2023-11-29 09:10:59 -0800

    Update App.js
    change display

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn devops for ci cd
+          Hi , How are You!
         </a>
       </header>
     </div>
