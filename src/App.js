9c0784b693f245c12a74e944d08b489fe905b915
abdirohman mekonen <abdi@Abd1.abdi>
2023-12-15 12:39:29 -0500

    5th commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to Ethiopia</h1>
+        <h1> Welcome to Ethiopia  hhh </h1>
       </header>
     </div>
   );
 }
-
 export default App;
