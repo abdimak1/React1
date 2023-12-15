a5b3c647a1bcde7a0dd56dc4a93a516723014c4d
abdirohman mekonen <abdi@Abd1.abdi>
2023-12-15 12:34:30 -0500

    5th commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome </h1>
+        <h1>Welcome to Ethiopia</h1>
       </header>
     </div>
   );
